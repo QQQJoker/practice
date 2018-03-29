a10bac381750a104b6c1e56731079a6713b2991f
joker <1047732652@qq.com>
2018-03-29 20:35:51 +0800

    joker:add test code for ThreadLocal count the method cost

---
+++ bookPractice/src/main/java/com/joker/practice/atomic/ThreadLocalTest.java
@@ -0,0 +1,31 @@
+package com.joker.practice.atomic;
+
+public class ThreadLocalTest {
+
+	private static final ThreadLocal<Long> TIME_THREADLOCAL = new ThreadLocal<Long>() {
+
+		@Override
+		protected Long initialValue() {
+			return System.currentTimeMillis();
+		}
+		
+	};
+	
+	public static void main(String[] args) {
+		ThreadLocalTest.begin();
+		
+		SleepUtils.second(1);
+		
+		System.out.println("Cost: "+ThreadLocalTest.end()+" mills");
+		
+	}
+	
+	public static final void begin() {
+		TIME_THREADLOCAL.set(System.currentTimeMillis());
+	}
+	
+	public static final long end() {
+		return System.currentTimeMillis()-TIME_THREADLOCAL.get();
+	}
+
+}
